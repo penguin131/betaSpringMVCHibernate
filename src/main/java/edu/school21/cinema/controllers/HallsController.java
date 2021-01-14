00a6dad3d72965793007cc64e3fddd0f8ddd6d1a
penguin131 <zininlol@rambler.ru>
2021-01-14 16:53:05 +0300

    fix versions!!!

--- src/main/java/edu/school21/cinema/controllers/HallsController.java
+++ src/main/java/edu/school21/cinema/controllers/HallsController.java
@@ -1,13 +1,15 @@
 package edu.school21.cinema.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/admin/panel/halls")
 public class HallsController {
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    @ResponseBody
     public String getAllHalls() {
         return "all halls";
     }
