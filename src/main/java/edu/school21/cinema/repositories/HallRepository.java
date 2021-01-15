504384eca1fd702377d5ba9d485bf3cd82db23f7
penguin131 <zininlol@rambler.ru>
2021-01-15 12:45:56 +0300

    custom repository

--- src/main/java/edu/school21/cinema/repositories/HallRepository.java
+++ src/main/java/edu/school21/cinema/repositories/HallRepository.java
@@ -1,9 +1,9 @@
 package edu.school21.cinema.repositories;
 
 import edu.school21.cinema.models.Hall;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface HallRepository extends CrudRepository<Hall, Long> {
+import java.util.List;
+
+public interface HallRepository {
+    List<Hall> findAll();
 }
